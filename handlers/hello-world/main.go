20e23c9b47b64a2fec94afc399f8cc5855a6bb2c
Yesarela Ritonga <yezarela@gmail.com>
2019-10-09 23:05:19 +0700

    feat: add unit test example

--- handlers/hello-world/main.go
+++ handlers/hello-world/main.go
@@ -1,18 +1,49 @@
 package main
 
 import (
-	"context"
+	"errors"
+	"fmt"
+	"io/ioutil"
+	"net/http"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
-	"github.com/yezarela/go-lambda/models"
-
-	_ "github.com/go-sql-driver/mysql"
 )
 
-func handler(ctx context.Context, req events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+var (
+	// DefaultHTTPGetAddress Default Address
+	DefaultHTTPGetAddress = "https://checkip.amazonaws.com"
 
-	return models.APIResponse(200, "Hello world!")
+	// ErrNoIP No IP found in response
+	ErrNoIP = errors.New("No IP in HTTP response")
+
+	// ErrNon200Response non 200 status code in response
+	ErrNon200Response = errors.New("Non 200 Response found")
+)
+
+func handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	resp, err := http.Get(DefaultHTTPGetAddress)
+	if err != nil {
+		return events.APIGatewayProxyResponse{}, err
+	}
+
+	if resp.StatusCode != 200 {
+		return events.APIGatewayProxyResponse{}, ErrNon200Response
+	}
+
+	ip, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return events.APIGatewayProxyResponse{}, err
+	}
+
+	if len(ip) == 0 {
+		return events.APIGatewayProxyResponse{}, ErrNoIP
+	}
+
+	return events.APIGatewayProxyResponse{
+		Body:       fmt.Sprintf("Hello, %v", string(ip)),
+		StatusCode: 200,
+	}, nil
 }
 
 func main() {
